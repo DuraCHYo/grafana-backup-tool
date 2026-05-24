d7e4b5dc85a20f14f924e91bdaa0a1a51719abe3
Danila Epishev <mrgame961@gmail.com>
2026-05-24 22:09:14 +0300

    feat: implement dataclasses structure for keeping setting in-memory (#17)
    Co-authored-by: Danila Epishev <DEpishev@alfabank.ru>

---
+++ grafana_backup/components/settings.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class CoreSettings:
+    url: str
+    headers: dict
+    verify_ssl: bool = False
+    client_cert: str = ""
+    debug: bool = False
+    search_api_limit: int = 5000
+    default_user_password: str = "00000000"
+    version: int = 0
+    admin_account: str = ""
+    admin_password: str = ""
+
+
+@dataclass
+class GeneralSettings:
+    debug: bool = True
+    api_health_check: bool = True
+    api_auth_check: bool = True
+    backup_dir: str = "_OUTPUT_"
+    backup_file_format: str = "%Y-%m-%d-%H-%M"
+    uid_dashboard_slug_suffix: bool = False
+    backup_workers: int = 3
+
+
+@dataclass
+class Aws:
+    s3_bucket_name: str = ""
+    s3_bucket_key: str = ""
+    default_region: str = ""
+    access_key_id: str = ""
+    secret_access_key: str = ""
+    endpoint_url: str = ""
+
+
+@dataclass
+class Azure:
+    connection_string: str = ""
+    container_name: str = ""
+
+
+@dataclass
+class GCP:
+    connection_string: str = ""
+    bucket_name: str = ""
